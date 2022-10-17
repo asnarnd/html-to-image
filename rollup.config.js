649868b709ba81f4ac414aa8ec98114391a2f641
Jorge V. Contreras <jcontreras@asna.com>
2022-10-17 18:19:28 -0500

    ES6 can use cleaner async functions and await.

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import config from '@bubkoo/rollup-config'
 
 export default config({
+  input: './es6',
   output: [
     {
       name: 'htmlToImage',
-      format: 'umd',
+      format: 'es',
       file: 'dist/html-to-image.js',
-      sourcemap: true,
+      sourcemap: false
     },
   ],
 })
